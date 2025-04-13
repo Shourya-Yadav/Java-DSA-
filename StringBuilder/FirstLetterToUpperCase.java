81c8f05c3a5deea897966f606508987d2d870aae
Shourya Yadav <shourya00212@gmail.com>
2025-04-14 00:08:22 +0530

    StringBuilder concepts

---
+++ StringBuilder/FirstLetterToUpperCase.java
@@ -0,0 +1,29 @@
+package StringBuilder;
+
+/*
+  <CODE_NATION>
+  Q->for a given String convert each the first letter of each word to uppercase
+  "hi,i am shourya"
+ */
+public class FirstLetterToUpperCase {
+        public static String toUpperCase(String str){
+            StringBuilder sb=new StringBuilder("");
+            char ch=Character.toUpperCase(str.charAt(0));
+            sb.append(ch);
+
+            for(int i=1; i < str.length();i++){
+               if(str.charAt(i) == ' ' && i < str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+               }else{
+                sb.append(str.charAt(i));
+               }
+            }
+            return sb.toString();
+        }
+        public static void main(String[] args) {
+        String str = "hi, i am shourya";
+        System.out.println(toUpperCase(str));
+    }
+}
