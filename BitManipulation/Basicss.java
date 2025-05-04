3201332cb18a74f9c6bf9bfbe22ebc2828e73d7c
Shourya Yadav <shourya00212@gmail.com>
2025-05-04 17:21:51 +0530

    Bit Manipulation

---
+++ BitManipulation/Basicss.java
@@ -0,0 +1,18 @@
+package BitManipulation;
+
+public class Basicss {
+    public static void main(String[] args) {
+        // System.out.println("Binary AND of 5 & 6");
+        // System.out.println(5 & 6);
+        // System.out.println("Binary OR of 5 & 6");
+        // System.out.println(5 | 6);
+        // System.out.println("Binary XOR of 5 & 6");
+        // System.out.println(5 ^ 6);
+        // System.out.println("Binary 1's complement of 5 & 6");
+        // System.out.println(~5);
+        System.out.println("Binary left shift of 5 with 2 places");
+        System.out.println(5 << 2);
+        System.out.println("Binary right shift of 6 with 1 place");
+        System.out.println(6 >> 1);
+    }
+}
